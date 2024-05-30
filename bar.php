c2c3a9df8bdba2f75660d2d88b67f1feff389aec
KovasMcCann <KovasMcCann@outlook.com>
2024-05-29 21:07:11 -0500

    Rebuilt to Apply new changes

--- bar.php
+++ bar.php
@@ -11,6 +11,6 @@
 
 <header>
 <h1 style="font-size: 2vw; border-bottom: solid #ddd 1px; margin: 0; position: fixed; width: 100%; top: 0; left: 0; right: 0; background-color: #02000a; margin: 0; padding: 0; ">
-     <a href="../index.php" style="text-decoration: none; color: #ff11f2;">6616</a> | <a href="../cyberia.php" style="text-decoration: none;">Cyberia</a> <!-- | <a href="" style="text-decoration: none;">Void</a> --> | <a href="" style="text-decoration: none;">Voyager</a> <!-- | <a href="" style="text-decoration: none;">Space Agency</a> | <a href="" style="text-decoration: none;">Descent</a> | <a href="" style="text-decoration: none;">Embasador</a> -->
+     <a href="../index.php" style="text-decoration: none; color: #6389cf;">Space</a> | <a href="../Tech.php" style="text-decoration: none;">Tech</a> <!-- | <a href="" style="text-decoration: none;">Void</a> --> | <a href="" style="text-decoration: none;">Voyager</a> <!-- | <a href="" style="text-decoration: none;">Space Agency</a> | <a href="" style="text-decoration: none;">Descent</a> | <a href="" style="text-decoration: none;">Embasador</a> -->
 </h1>
 </header>
--- index.php
+++ index.php
@@ -1,16 +1,30 @@
 <!doctype html>
-<title>District 6616</title>
+<title>4ᵗʰ Dimensonal Space</title>
 
 <?php include "bar.php"?>
 
-<main style="margin: auto">
+
+
+<main style="margin: 3em">
+
 <p style="font-size: 85px; text-align: center; margin: 3vw;">
-District 6616<span style="font-size: 85px;" class="blinking-cursor">|</span>
+4ᵗʰ Dimensonal Space<span style="font-size: 85px; " class="blinking-cursor">|</span>
 </p>
 
+
 <p style="text-align: center; margin-bottom: 0px;">
-A image board hosted on tor 
+Kovas McCann's Personal Webpage
 </p>
+
+<noscript>
+    <p style="text-align: center; margin-bottom: 0px;"> 
+        JS disabled! keep it up! 
+    </p>
+</noscript>
+
+<h2 style="float:right; unset;">
+<img src="../img/dimenson.png" class="ui-draggable" alt="Teseract" height="250" width="300" style=" border: 3px solid #808080; display: inline-block "/>
+</h2>
 
 
 <h3 style="font-size: 35px; background-color: #00050a;">
@@ -19,103 +33,12 @@ Rules
 
 <ul>
 <li>
-Don't post inaproprate content no henti, NSFW or links to such things.
-</li>
-<li>
-Be nice don't be a troll this is a community not a wastland
-</li>
-<li>
-Flooding is not permitted. We define flooding as posting similar posts more than 3 times per hour, making a thread on a topic for which a thread already exists, or posting in such a way that it significantly changes the composition of a board. Common sense will be utilized.
-</li>
-<li>
-Adhere to the same standards of behavior online that you follow in real life
-</li>
-<li>
-forgiv other people mistakes
+Kovas McCann Rock's
 </li>
 </ul>
-
-<h3 style="font-size: 35px; background-color: #00050a;">
-Boards
-</h3>
-
-<ul style=""> 
-<li>
-cyberia (tech)
-</li>
-<ul>
-<li>
-triangulate (privacy)
-</li>
-<li>
-Linux
-</li>
-<li>
-BSD
-</li>
-<li>
-News
-</li>
-<li>
-General
-</li>
-</ul>
-<li>
-Music
-</li>
-<li>
-void (lounge)
-</li>
-<li>
-voyeger (Webpage links)
-</li>
-<ul>
-<li>
-Clearnet
-</li>
-<li>
-Tor 
-</li>
-</ul>
-<li>
-Desecent (diving)
-</li>
-<li>
-Embasador (site updaates)
-</li>
-</ul>
-
-<h3 style="font-size: 35px; background-color: #00050a;">
-Stats
-</h3>
-
-<?php
- #Storage graph
- echo('<h2 style="color:  #1aff1a ">');
- echo('Storage');
- echo('<h2 style="color:  #1aff1a ">');
- exec("df -h | grep '/dev/sda3'  | awk '/^'/'dev {print $3 }' | sed 's/G//g'", $zzzz);
- exec("df -h | grep '/dev/sda3'  | awk '/^'/'dev {print $2 }' | sed 's/G//g'", $zzz);
- echo("$zzzz[0]G/$zzz[0]G");
-  echo('</h2>');
- echo('<meter id="storage" value="'.$zzzz[0].'" min="0" max="'.$zzz[0].'">2 out of 10</meter><br>');
- ?>
-
- <?php
- echo('<h2 style="color:  #1aff1a ">');
- echo('RAM');
- exec("free -h | awk '/^Mem:/ {print $2}' | sed 's/Gi//g'",$total);
- exec("free -h | awk '/^Mem:/ {print $3}' | sed 's/Gi//g'",$used);
- echo('</h2>');
- echo('<h2 style="color:  #1aff1a ">');
- echo("$used[0]Gi/$total[0]Gi");
- echo('</h2>');
- echo('<meter id="storage" value="'.$used[0].'" min="0" max="'.$total[0].'">2 out of 10</meter><br>');
-?>
-
-
 
 </main>
 </body>
 <footer>
+    <p>Kovas McCann | 2024 </p>
 </footer>
