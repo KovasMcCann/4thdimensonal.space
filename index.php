5059122f2cfcbf284a48ff19df13503e3ec3e8c6
Kovas McCann <Kovasmccann@outlook.com>
2024-06-06 18:18:47 +0000

    Fixed

--- index.php
+++ index.php
@@ -21,11 +21,11 @@ Kovas McCann's Personal Webpage
         JS disabled! keep it up! 
     </p>
 </noscript>
-
+<!--
 <h2 style="float:right; unset;">
 <img src="../img/dimenson.png" class="ui-draggable" alt="Teseract" height="250" width="300" style=" border: 3px solid #808080; display: inline-block "/>
 </h2>
-
+-->
 
 <h3 style="font-size: 35px; background-color: #00050a;">
 Rules
@@ -40,7 +40,11 @@ Kovas McCann Rock's
 </main>
 </body>
 <footer >
-        <h3 style="font-size: 2vw; border-top: solid #ddd 1px; margin: 0; position: fixed; width: 100%;  margin: 0; padding: 0; display: inline; ">
-        <p>Kovas McCann | XMR: </p> <p style="font-size:1vw">45yP52mp6Hb9v3TQNrMeihQAJjVVUGacdEidm7rCUSNB8dWuU9QV3amB87xGhoEWioQEMBmVEyXBF1j2Mb4M486gMS3fnqN</p> <p> | 2024 </p>
-        </h3>
+    <center>
+    <h3 style="font-size: 2vw; border-top: solid #ddd 1px; margin: 0; position: fixed; width: 100%; padding: 0;">
+        <p style="display: inline; margin: 0;">Kovas McCann | XMR: </p> 
+        <p style="display: inline; font-size:1vw; margin: 0;">45yP52mp6Hb9v3TQNrMeihQAJjVVUGacdEidm7rCUSNB8dWuU9QV3amB87xGhoEWioQEMBmVEyXBF1j2Mb4M486gMS3fnqN</p> 
+        <p style="display: inline; margin: 0;"> | 2024 </p>
+    </h3>
+</center>
 </footer>
